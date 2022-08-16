cc8d9ba9f84e3d9fb6cd159b3e28a0c83f5f196e
lok6666 <1239094016@qq.com>
2022-08-16 22:23:48 +0800

    政策标签接口

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,6 +1,6 @@
 import {Module} from "vuex";
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { get, post } from "@/utils/request";
+import { get, post, deleteItem } from "@/utils/request";
 import {
     login,
   } from "@/config/api";
@@ -30,20 +30,55 @@ const actions = {
     login({ commit,dispatch }, userInfo) {
         const { username, password } = userInfo
         return new Promise(async (resolve, reject) => {
+            // post(`http://172.16.130.92:28191/policyRelation/insert`, {
+            //     policyId: 1,
+            //     noticeId: 2,
+            //     startTime: '2022-08-09 12:00:00',
+            //     endTime: '2022-08-09 12:00:00',
+            //     lastUpdataTime: '2022-08-09 12:00:00',
+            //     storageTime: '2022-08-09 12:00:00'
+            // }).then(async function (data) {
+            //    console.log('data', data);
+            // });
+            post(`http://172.16.130.92:28191/policyRelation/list`, {
+                id: 1312,
+                pageNum: 0,
+                pageSize: 10
+            }).then(async function (data) {
+               console.log('data', data);
+               post(`http://172.16.130.92:28191/policyRelation/update`, {
+                id: 1304,
+                policyId: 4,
+                noticeId: 4
+              }).then(function (data) {
+                post(`http://172.16.130.92:28191/policyRelation/list`, {
+                    id: 1312,
+                    pageNum: 0,
+                    pageSize: 10
+                }).then(async function (data) {
+                    post(`http://172.16.130.92:28191/policyTags/listNoPage`, {
+                        id: 1312
+                    }).then(async function (data) {
+                    console.log('data', data);
+                    });
+                });
+              });
+            });
             // 登录接口
-             post(`${login}`, {
-                 headers: {
-                 "Access-Control-Allow-Origin": "*",
-                 },
-                 username: "admin",
-                 password: "12345"
-             }).then(async function ({token, userId}) {
-                commit('SET_TOKEN', token)
-                // commit('SET_USERID', userId)
-                await dispatch('getInfo', ['admin']) // 获取权限列表 默认就是超级管理员，因为没有进行接口请求 写死
-                setToken(token)
-                resolve(token)
-             });
+            //  post(`${login}`, {
+            //      headers: {
+            //      "Access-Control-Allow-Origin": "*",
+            //      },
+            //      username: "admin",
+            //      password: "12345"
+            //  }).then(async function ({token, userId}) {
+            //     debugger;
+            //     commit('SET_TOKEN', token)
+            //     // commit('SET_USERID', userId)
+            //     await dispatch('getInfo', ['admin']) // 获取权限列表 默认就是超级管理员，因为没有进行接口请求 写死
+            //     setToken(token)
+            //     resolve(token)
+            //  });
         })
     },
     // 获取用户信息 ，如实际应用中 可以通过token通过请求接口在这里获取用户信息
